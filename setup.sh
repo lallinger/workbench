55cc1d50f3236539447f421a931a666f3229f4e3
Patrick <patrick@lallinger.tech>
2025-07-31 12:02:59 +0000

    vim yaml support

--- setup.sh
+++ setup.sh
@@ -448,6 +448,46 @@ function neovim_install() {
   },
 }' >$HOME/.config/nvim/lua/plugins/toggle-paste.lua
 
+  echo 'return {
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    ft = { "yaml" },
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function(_, opts)
+      local cfg = require("yaml-companion").setup(opts)
+      require("lspconfig")["yamlls"].setup(cfg)
+      require("telescope").load_extension("yaml_schema")
+    end,
+  },
+}' >$HOME/.config/nvim/lua/plugins/yaml-companion.lua
+
+  echo 'return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function(_, opts)
+    opts.sections.lualine_z = {
+      {
+        function()
+          local schema = require("yaml-companion").get_buf_schema(0)
+          if schema.result[1].name == "none" then
+            return ""
+          end
+          return schema.result[1].name
+        end,
+      },
+      {
+        function()
+          return " " .. os.date("%R")
+        end,
+      },
+    }
+  end,
+}' >$HOME/.config/nvim/lua/plugins/lualine.lua
+
   # open issue: https://github.com/LazyVim/LazyVim/issues/6039
   echo 'return {
   { "mason-org/mason.nvim", version = "^1.0.0" },
@@ -545,7 +585,7 @@ install_tools() {
   fuck_install
   #xxh_install
   speedtest_install
-  operator_sdk_install
+  #operator_sdk_install
   argocd_install
   virtctl_install
   neovim_install
