ce99fa885893c36f8c5bd44a9c81d4a5ae1201b4
Patrick <patrick@lallinger.tech>
2025-08-20 16:41:29 +0000

    add env

--- setup.sh
+++ setup.sh
@@ -684,8 +684,9 @@ function bitwarden_install() {
   mv -f completion_bitwarden $COMPLETION_FOLDER/bitwarden
   # set BWS_ACCESS_TOKEN in ~/.secure_vars !
   add_to_profile bitwarden "source $COMPLETION_FOLDER/bitwarden
-  export OPENAI_API_KEY=\$(bws secret list | yq e '.[] | select(.key == \"openai-api-key\") | .value')
-  export PASSWORD=\$(bws secret list | yq e '.[] | select(.key == \"password\") | .value')"
+export OPENAI_API_KEY=\$(bws secret list | yq e '.[] | select(.key == \"openai-api-key\") | .value')
+export PASSWORD=\$(bws secret list | yq e '.[] | select(.key == \"password\") | .value')
+export TF_VAR_password=\$PASSWORD"
 }
 
 function miscelanious_install() {
