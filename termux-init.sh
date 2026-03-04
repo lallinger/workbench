183f5476a759fb8f8bef0aa9ab796b8e5a5ff8a5
Patrick <patrick@lallinger.tech>
2026-03-04 20:04:40 +0100

    improve termux init

--- termux-init.sh
+++ termux-init.sh
@@ -1,6 +1,12 @@
 pushd $HOME
 cp -rT storage/downloads/home/ .
+apt install -y git
+pushd gitops
+git pull
+popd
+pushd talos
+git pull
+popd
 pushd workbench/
 chmod +x ./setup.sh
-./setup.sh 
-
+./setup.sh
